b686373306139a343ec1297e8166e1ac27b7ff81
SrStamm <mirkostamm19@gmail.com>
2025-11-06 19:28:43 +0000

    feat: dashboard layout basic

---
+++ front-tasks/src/layouts/DashboardLayout.tsx
@@ -0,0 +1,50 @@
+import { FaTasks } from "react-icons/fa";
+import { RxDashboard } from "react-icons/rx";
+import { GoProject } from "react-icons/go";
+import { IoChatboxEllipsesOutline } from "react-icons/io5";
+import { FaUserGroup } from "react-icons/fa6";
+import { FaRegUserCircle } from "react-icons/fa";
+
+function DashboardLayout({ children }) {
+  return (
+    <div className="principal-layout">
+      <aside className="sidebar-container">
+        <div className="sidebar">
+          <h1>Project Manager</h1>
+          <nav>
+            <div>
+              <div className="sidebar_element">
+                <FaUserGroup />
+                <span>"Grupo actual"</span>
+              </div>
+              <div className="sidebar_element">
+                <RxDashboard />
+                <span>Dashboard</span>
+              </div>
+              <div className="sidebar_element">
+                <GoProject />
+                <span>Proyectos</span>
+              </div>
+              <div className="sidebar_element">
+                <FaTasks />
+                <span>Tareas</span>
+              </div>
+              <div className="sidebar_element">
+                <IoChatboxEllipsesOutline /> Chat
+              </div>
+            </div>
+          </nav>
+        </div>
+        <div>
+          <div className="sidebar_element">
+            <FaRegUserCircle /> User
+          </div>
+        </div>
+      </aside>
+
+      <main className="dashboard-layout">{children}</main>
+    </div>
+  );
+}
+
+export default DashboardLayout;
