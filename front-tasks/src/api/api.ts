55ad6ba30cd6778e21a64a449223cb6f458ce3b2
SrStamm <mirkostamm19@gmail.com>
2025-11-12 15:10:49 +0000

    implemented Fetch function

---
+++ front-tasks/src/api/api.ts
@@ -0,0 +1,28 @@
+const url = import.meta.env.URL;
+
+type FetchProps = {
+  path: string;
+  method: string;
+  body?: object;
+};
+
+const Fetch = async ({ path, method, body }: FetchProps) => {
+  const token = localStorage.getItem("auth_token");
+
+  const Body = body !== undefined ? JSON.stringify(body) : undefined;
+
+  const fetchOptions: RequestInit = {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + token,
+    },
+    ...(Body && { body: Body }),
+  };
+
+  const response = await fetch(url + path, fetchOptions);
+
+  return response;
+};
+
+export default Fetch;
