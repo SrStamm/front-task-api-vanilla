57d6d708031064cf5efd7973f6cc22147deb7d38
SrStamm <mirkostamm19@gmail.com>
2025-11-12 16:10:26 +0000

    feat: implemented user validation to public and private routes

--- front-tasks/src/routes/AppRouter.tsx
+++ front-tasks/src/routes/AppRouter.tsx
@@ -1,12 +1,43 @@
 // Definition for public and private routes
 
-import App from "../App";
 import { createBrowserRouter } from "react-router-dom";
 import LoginPage from "../pages/LoginPage";
 import RegisterPage from "../pages/RegisterPage";
+import { AuthProvider } from "../providers/AuthProvider";
+import { PublicRoute } from "./PublicRoute";
+import { PrivateRoute } from "./ProtectedRoute";
+import DashboardLayout from "../layouts/DashboardLayout";
+import "../App.css";
 
 export const router = createBrowserRouter([
-  { path: "/", element: <App /> },
-  { path: "/login", element: <LoginPage /> },
-  { path: "/register", element: <RegisterPage /> },
+  {
+    path: "/login",
+    element: (
+      <AuthProvider>
+        <PublicRoute>
+          <LoginPage />
+        </PublicRoute>
+      </AuthProvider>
+    ),
+  },
+  {
+    path: "/register",
+    element: (
+      <AuthProvider>
+        <PublicRoute>
+          <RegisterPage />
+        </PublicRoute>
+      </AuthProvider>
+    ),
+  },
+  {
+    path: "/dashboard",
+    element: (
+      <AuthProvider>
+        <PrivateRoute>
+          <DashboardLayout />
+        </PrivateRoute>
+      </AuthProvider>
+    ),
+  },
 ]);
