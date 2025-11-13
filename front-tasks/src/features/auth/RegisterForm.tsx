59dd415251ccf1f288cf0e3f536a2476c5c73f38
SrStamm <mirkostamm19@gmail.com>
2025-11-13 15:16:24 +0000

    feat: implemented register events

--- front-tasks/src/features/auth/RegisterForm.tsx
+++ front-tasks/src/features/auth/RegisterForm.tsx
@@ -1,11 +1,62 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import Button from "../../components/common/Button/index.tsx";
+import { useState } from "react";
+import type { CreateUser } from "../../types/User.ts";
+
+const url = import.meta.env.VITE_URL;
 
 function RegisterForm() {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  const usernameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(e.target.value);
+  };
+
+  const emailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEmail(e.target.value);
+  };
+
+  const passwordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newUserData: CreateUser = {
+      username: username,
+      email: email,
+      password: password,
+    };
+
+    try {
+      const res = await fetch(url + "user", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newUserData),
+      });
+
+      if (res.ok) {
+        navigate("/login");
+      } else {
+        const dataError = await res.json();
+        console.log("Error: ", dataError.detail);
+      }
+    } catch (error) {
+      console.error("Error al intentar conectar con el servidor:", error);
+    }
+  };
+
   return (
     <>
       <h2>Registro</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <label className="form-label">Username:</label>
           <input
@@ -13,6 +64,8 @@ function RegisterForm() {
             className="input-base"
             placeholder="Ingresa un nombre de usuario"
             required
+            value={username}
+            onChange={usernameChange}
           />
         </div>
         <div className="form-group">
@@ -23,6 +76,8 @@ function RegisterForm() {
             placeholder="Ingrese su email"
             required
             minLength={6}
+            value={email}
+            onChange={emailChange}
           />
         </div>
         <div className="form-group">
@@ -30,7 +85,9 @@ function RegisterForm() {
           <input
             type="password"
             className="input-base"
-            placeholder="Ingresa una nueva contraseña"
+            placeholder="Ingresa una contraseña"
+            value={password}
+            onChange={passwordChange}
           />
         </div>
         <div className="btn-container">
