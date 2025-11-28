396592d85b25f4e8c80f85ee05b68eacaef10eda
SrStamm <mirkostamm19@gmail.com>
2025-11-28 11:15:39 +0000

    feat: ProjectsPage created and implemented

--- front-tasks/src/routes/dashboard.routes.tsx
+++ front-tasks/src/routes/dashboard.routes.tsx
@@ -1,8 +1,12 @@
 import DashboardLayout from "../layouts/DashboardLayout";
 import GroupsPage from "../pages/Dashboard/GroupsPage";
+import ProjectsPage from "../pages/Dashboard/ProjectsPage";
 
 export const dashboardRoutes = {
   path: "/dashboard",
   element: <DashboardLayout />,
-  children: [{ path: "groups", element: <GroupsPage /> }],
+  children: [
+    { path: "groups", element: <GroupsPage /> },
+    { path: "projects", element: <ProjectsPage /> },
+  ],
 };
