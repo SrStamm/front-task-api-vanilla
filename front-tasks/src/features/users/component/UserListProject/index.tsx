ad5415375b6d82517d3cbd8b33466542114e1cb1
SrStamm <mirkostamm19@gmail.com>
2025-11-28 23:20:37 +0000

    fix: move actions into .map()

--- front-tasks/src/features/users/component/UserListProject/index.tsx
+++ front-tasks/src/features/users/component/UserListProject/index.tsx
@@ -5,29 +5,40 @@ import "./UserListProject.css";
 
 interface userListProps {
   users: UserInProject[];
+  groupId: number;
+  projectId: number;
+  onDelete: (group_id: number, project_id: number, user_id: number) => void;
 }
 
-function UserListProject({ users }: userListProps) {
-  const actions = (
-    <>
-      <select
-        className="permission-select"
-        disabled={true}
-        style={{ display: "none" }}
-      >
-        <option value="read">Lectura</option>
-        <option value="write">Escritura</option>
-        <option value="admin">Administrador</option>
-      </select>
-
-      <Button className="btn-vsm btn-outline-secondary" text="Editar" />
-      <Button className="btn-vsm btn-outline-error" text="Eliminar" />
-    </>
-  );
-
+function UserListProject({
+  users,
+  groupId,
+  projectId,
+  onDelete,
+}: userListProps) {
   return (
     <ul className="listUser">
       {users.map((u) => {
+        const actions = (
+          <>
+            <select
+              className="permission-select"
+              disabled={true}
+              style={{ display: "none" }}
+            >
+              <option value="read">Lectura</option>
+              <option value="write">Escritura</option>
+              <option value="admin">Administrador</option>
+            </select>
+
+            <Button className="btn-vsm btn-outline-secondary" text="Editar" />
+            <Button
+              className="btn-vsm btn-outline-error"
+              text="Eliminar"
+              onClick={() => onDelete(groupId, projectId, u.user_id)}
+            />
+          </>
+        );
         return (
           <li key={u.user_id} className="user-item">
             <UserCard type="project" userProject={u} actions={actions} />
