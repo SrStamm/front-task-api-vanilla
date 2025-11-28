1cd6485b1285877b5390cb741b60347da87a0dce
SrStamm <mirkostamm19@gmail.com>
2025-11-28 16:42:05 +0000

    feat: actions implemented for UserListProject

--- front-tasks/src/features/users/component/UserListProject/index.tsx
+++ front-tasks/src/features/users/component/UserListProject/index.tsx
@@ -1,3 +1,4 @@
+import Button from "../../../../components/common/Button";
 import type { UserInProject } from "../../../projects/schemas/Project";
 import UserCard from "../UserCard";
 import "./UserListProject.css";
@@ -7,13 +8,34 @@ interface userListProps {
 }
 
 function UserListProject({ users }: userListProps) {
-  return users.map((u) => {
-    return (
-      <li className="user-item">
-        <UserCard type="project" userProject={u} />
-      </li>
-    );
-  });
+  const actions = (
+    <>
+      <select
+        className="permission-select"
+        disabled={true}
+        style={{ display: "none" }}
+      >
+        <option value="read">Lectura</option>
+        <option value="write">Escritura</option>
+        <option value="admin">Administrador</option>
+      </select>
+
+      <Button className="btn-vsm btn-outline-secondary" text="Editar" />
+      <Button className="btn-vsm btn-outline-error" text="Eliminar" />
+    </>
+  );
+
+  return (
+    <ul className="listUser">
+      {users.map((u) => {
+        return (
+          <li key={u.user_id} className="user-item">
+            <UserCard type="project" userProject={u} actions={actions} />
+          </li>
+        );
+      })}
+    </ul>
+  );
 }
 
 export default UserListProject;
