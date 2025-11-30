51fde097ebb96ec71b92a821a7cc3450c47ee503
SrStamm <mirkostamm19@gmail.com>
2025-11-30 18:41:22 +0000

    feat: function to add user to project in modal

--- front-tasks/src/features/users/component/UserAddModal/index.tsx
+++ front-tasks/src/features/users/component/UserAddModal/index.tsx
@@ -1,17 +1,37 @@
 import Button from "../../../../components/common/Button";
 import Modal from "../../../../components/common/Modal";
 import type { UserInGroup } from "../../../groups/schemas/Group";
+import { useProjects } from "../../../projects/hooks/useProject";
+import type { ReadProject } from "../../../projects/schemas/Project";
 import UserListGroup from "../UserListGroup";
 
 interface userAddModalProps {
   show: boolean;
   onClose: () => void;
   usersInGroup: UserInGroup[];
+  project: ReadProject;
 }
 
-function UserAddModal({ show, onClose, usersInGroup }: userAddModalProps) {
+function UserAddModal({
+  show,
+  onClose,
+  usersInGroup,
+  project,
+}: userAddModalProps) {
+  const { addUserToProject } = useProjects();
+
+  const handleAddUserToProject = async (userId: number) => {
+    await addUserToProject(project.group_id, project.project_id, userId);
+  };
+
   const header = <h2>Lista de usuarios</h2>;
-  const body = <UserListGroup users={usersInGroup} addUser={true} />;
+  const body = (
+    <UserListGroup
+      users={usersInGroup}
+      addUser={true}
+      onAdd={handleAddUserToProject}
+    />
+  );
   const actions = (
     <>
       <Button text="cancelar" className="btn-primary" onClick={onClose} />
