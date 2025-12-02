ab1ae9f2c851b51024752b1562005a46be2e6440
SrStamm <mirkostamm19@gmail.com>
2025-12-02 11:37:54 +0000

    feat: load the server tasks and separates them by column

--- front-tasks/src/features/tasks/components/KanbanBoard/index.tsx
+++ front-tasks/src/features/tasks/components/KanbanBoard/index.tsx
@@ -2,34 +2,33 @@
 
 import "./KanbanBoard.css";
 import Column from "../Column";
-
-const tasks = [
-  {
-    task_id: 1,
-    project_id: 2,
-    title: "probando",
-    description: "probando",
-    date_exp: "2025-12-12",
-    state: "En progreso",
-    task_labels_links: [],
-    asigned: [
-      {
-        user_id: 1,
-        username: "test",
-      },
-    ],
-  },
-];
+import { useTasks } from "../../hooks/useTasks";
 
 function KanbanBoard() {
+  const { tasksInProject, isLoading, error } = useTasks();
+
+  if (isLoading) return <p style={{ textAlign: "center" }}>Cargando...</p>;
+  if (error)
+    return (
+      <p
+        style={{ textAlign: "center", color: "red" }}
+      >{`Error al cargar las tareas: ${error}`}</p>
+    );
+
+  const todoTasks = tasksInProject.filter((t) => t.state === "sin empezar");
+  const inProgressTasks = tasksInProject.filter(
+    (t) => t.state === "en proceso",
+  );
+  const doneTasks = tasksInProject.filter((t) => t.state === "completado");
+
+  console.log(todoTasks);
+
   return (
-    <>
-      <div className="task-container">
-        <Column column_text="To Do" tasks={tasks} />
-        <Column column_text="In Progress" tasks={tasks} />
-        <Column column_text="Done" tasks={tasks} />
-      </div>
-    </>
+    <div className="task-container">
+      <Column column_text="To Do" tasks={todoTasks} />
+      <Column column_text="In Progress" tasks={inProgressTasks} />
+      <Column column_text="Done" tasks={doneTasks} />
+    </div>
   );
 }
 
