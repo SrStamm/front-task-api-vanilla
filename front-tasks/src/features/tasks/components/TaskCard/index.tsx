756c0e413242cbe6d7571945764ce1902a2f6c36
SrStamm <mirkostamm19@gmail.com>
2025-12-02 11:08:21 +0000

    feat: styles change with state card

--- front-tasks/src/features/tasks/components/TaskCard/index.tsx
+++ front-tasks/src/features/tasks/components/TaskCard/index.tsx
@@ -8,18 +8,30 @@ interface TaskCardProps {
 }
 
 function TaskCard({ task }: TaskCardProps) {
+  const dueDate = new Date(task.date_exp);
+  const options = { year: "numeric", month: "short", day: "numeric" };
+  const formatedDate = dueDate.toLocaleDateString("es-ES", options);
+
+  const state =
+    task.state == "En progreso"
+      ? "in-progress"
+      : task.state == "Completado"
+        ? "done"
+        : "todo";
   return (
-    <li className="task-card">
+    <li className={`task-card ${state}`}>
       <div className="task-card-details">
-        <div className="task-info">
-          <h3 className="task-title">{task.title}</h3>
+        <div className="task-card-meta">
+          <div className="task-info">
+            <h3 className="task-title">{task.title}</h3>
+          </div>
+          <div className={`task-state ${state}`}>{task.state}</div>
         </div>
+
         <div className="task-description">
           {task.description && task.description}
-        </div>
-        <div className="task-card-meta">
           <div>
-            <p className="task-date"> {task.date_exp}</p>
+            <p className="task-date"> {formatedDate}</p>
           </div>
           <div>
             <p className="task-label">
