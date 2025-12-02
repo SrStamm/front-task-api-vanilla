515e0069608da8ad6870b9428baeddb12d06e425
SrStamm <mirkostamm19@gmail.com>
2025-12-02 16:07:20 +0000

    feat: updated page structure

--- front-tasks/src/pages/Dashboard/TasksPage/TasksPage.tsx
+++ front-tasks/src/pages/Dashboard/TasksPage/TasksPage.tsx
@@ -4,13 +4,11 @@ import "./TasksPage.css";
 function TaskPage() {
   return (
     <section className="dashboard-section ">
-      <article className="partSections ">
-        <header className="headerPartSection">
-          <h3 className="dashboard-layout-h3">Tareas</h3>
-        </header>
+      <header className="headerPartSection">
+        <h3 className="dashboard-layout-h3">Tareas</h3>
+      </header>
 
-        <KanbanBoard />
-      </article>
+      <KanbanBoard />
     </section>
   );
 }
