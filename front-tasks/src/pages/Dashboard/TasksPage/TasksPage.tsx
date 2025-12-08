4b02c8d4a136d582f9b45dc63c828aafccaec1f8
SrStamm <mirkostamm19@gmail.com>
2025-12-08 21:06:41 +0000

    feat: implemented modal in TaskPage

--- front-tasks/src/pages/Dashboard/TasksPage/TasksPage.tsx
+++ front-tasks/src/pages/Dashboard/TasksPage/TasksPage.tsx
@@ -1,14 +1,36 @@
+import { useState } from "react";
+import CreateTaskModal from "../../../features/tasks/components/CreateTaskModal";
 import KanbanBoard from "../../../features/tasks/components/KanbanBoard";
 import "./TasksPage.css";
+import Button from "../../../components/common/Button";
 
 function TaskPage() {
+  const [showCreateModal, setShowCreateModal] = useState(false);
+
+  const handleOpenCreateModal = () => {
+    setShowCreateModal(true);
+  };
+
+  const handleCloseCreateModal = () => {
+    setShowCreateModal(false);
+  };
+
   return (
     <section className="dashboard-section ">
       <header className="headerPartSection">
         <h3 className="dashboard-layout-h3">Tareas</h3>
+        <Button
+          className="btn-primary"
+          text="+"
+          onClick={handleOpenCreateModal}
+        />
       </header>
 
       <KanbanBoard />
+      <CreateTaskModal
+        showModal={showCreateModal}
+        onClose={handleCloseCreateModal}
+      />
     </section>
   );
 }
