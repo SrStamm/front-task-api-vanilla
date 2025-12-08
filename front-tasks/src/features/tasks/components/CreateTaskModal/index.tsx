bff42dec504a9d7ca50b733f34692304c6c25266
SrStamm <mirkostamm19@gmail.com>
2025-12-08 20:53:56 +0000

    feat: CreateTaskModal created

---
+++ front-tasks/src/features/tasks/components/CreateTaskModal/index.tsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import Button from "../../../../components/common/Button";
+import Modal from "../../../../components/common/Modal";
+import { useGroupProject } from "../../../../hooks/useGroupProject";
+import { useProjects } from "../../../projects/hooks/useProject";
+import UserList from "../../../users/component/UserList";
+import type { ReadUser } from "../../../../types/User.ts";
+
+interface CreateTaskModalProps {
+  showModal: boolean;
+}
+
+function CreateTaskModal({ showModal }: CreateTaskModalProps) {
+  const [usersSelected, setUsersSelected] = useState<ReadUser[]>([]);
+  const { projects } = useProjects();
+  const { projectId } = useGroupProject();
+
+  useEffect(() => {
+    if (projectId && projects) {
+      const project = projects.find((p) => p.project_id == projectId);
+
+      setUsersSelected(project ? project.users : []);
+    }
+  }, [projectId, projects]);
+
+  const header = <h2>Crear una nueva tarea</h2>;
+
+  const body = (
+    <form id="create-task-form">
+      <label>Titulo:</label>
+      <input type="text" className="input-base" />
+
+      <label>Descripción:</label>
+      <textarea
+        rows={3}
+        cols={3}
+        className="input-base small-textarea"
+      ></textarea>
+
+      <label>Fecha de vencimiento:</label>
+      <input type="date" className="input-base" required />
+
+      <div className="user-selection-container">
+        <label> Asignar a usuarios: </label>
+        <ul>
+          <UserList users={usersSelected} onAdd={() => console.log("hola")} />
+        </ul>
+      </div>
+    </form>
+  );
+
+  const actions = (
+    <>
+      <Button className="btn-primary btn-sm" text="Confirmar" />
+      <Button className="btn-error btn-sm" text="Cancelar" />
+    </>
+  );
+
+  return (
+    <Modal
+      showModal={showModal}
+      size="large"
+      modalHeader={header}
+      modalBody={body}
+      modalActions={actions}
+    />
+  );
+}
+
+export default CreateTaskModal;
