ace13827f5df13d18c0581402b0776a5979698d6
SrStamm <mirkostamm19@gmail.com>
2025-12-17 18:50:09 +0000

    feat: updated Sidebar and dashboard.routes to implement ChatPage

--- front-tasks/src/routes/dashboard.routes.tsx
+++ front-tasks/src/routes/dashboard.routes.tsx
@@ -1,4 +1,5 @@
 import DashboardLayout from "../layouts/DashboardLayout/DashboardLayout";
+import ChatPage from "../pages/Dashboard/ChatPage/ChatPage.tsx";
 import GroupsPage from "../pages/Dashboard/GroupsPage";
 import ProjectsPage from "../pages/Dashboard/ProjectsPage";
 import TasksPage from "../pages/Dashboard/TasksPage/TasksPage.tsx";
@@ -10,5 +11,6 @@ export const dashboardRoutes = {
     { path: "groups", element: <GroupsPage /> },
     { path: "projects", element: <ProjectsPage /> },
     { path: "tasks", element: <TasksPage /> },
+    { path: "chat", element: <ChatPage /> },
   ],
 };
