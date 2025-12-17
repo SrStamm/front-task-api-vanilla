2519a84ba030d479059165e206c588cd13f51c64
SrStamm <mirkostamm19@gmail.com>
2025-12-17 12:15:01 +0000

    feat: comment schema created

---
+++ front-tasks/src/features/comments/schemas.ts
@@ -0,0 +1,20 @@
+import { z } from "zod";
+
+export const CreateCommentSchema = z.object({
+  content: z.string(),
+});
+
+export const ReadCommentSchema = z.object({
+  comment_id: z.number(),
+  task_id: z.number(),
+  user_id: z.number(),
+  content: z.string(),
+  created_at: z.date(),
+  update_at: z.date(),
+  is_deleted: z.boolean(),
+});
+
+export const UpdateCommentSchema = z.object({
+  content: z.string().optional(), // max_length=300
+  is_deleted: z.boolean().optional(),
+});
