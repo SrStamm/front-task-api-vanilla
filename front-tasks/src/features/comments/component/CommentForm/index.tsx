61184d203f7d417642e5deb2a73d37e3ba6d3469
SrStamm <mirkostamm19@gmail.com>
2025-12-17 15:36:33 +0000

    feat: CommentForm styles updated

--- front-tasks/src/features/comments/component/CommentForm/index.tsx
+++ front-tasks/src/features/comments/component/CommentForm/index.tsx
@@ -4,6 +4,7 @@ import type {
   ReadCommentInterface,
 } from "../../schemas";
 import Button from "../../../../components/common/Button";
+import "./CommentForm.css";
 
 interface CommentFormProps {
   onSubmit: (payload: CreateCommentInterface) => Promise<ReadCommentInterface>;
@@ -21,7 +22,7 @@ function CommentForm({ onSubmit }: CommentFormProps) {
   };
 
   return (
-    <form id="form-comment" className="form-comment" onSubmit={handleSubmit}>
+    <form className="form-comment" onSubmit={handleSubmit}>
       <input
         className="input-base"
         type="text"
@@ -30,12 +31,7 @@ function CommentForm({ onSubmit }: CommentFormProps) {
         onChange={(e) => setMessage(e.target.value)}
       />
 
-      <Button
-        className="btn btn-primary btn-sm"
-        text="Enviar"
-        type="submit"
-        form="form-comment"
-      />
+      <Button className="btn btn-primary btn-sm" text="Enviar" type="submit" />
     </form>
   );
 }
