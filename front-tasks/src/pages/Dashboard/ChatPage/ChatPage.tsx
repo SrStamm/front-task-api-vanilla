27af3e99c69a28701ca971a0d726d397f4bb73a3
SrStamm <mirkostamm19@gmail.com>
2025-12-18 10:07:34 +0000

    feat: updated styles in ChatPage

--- front-tasks/src/pages/Dashboard/ChatPage/ChatPage.tsx
+++ front-tasks/src/pages/Dashboard/ChatPage/ChatPage.tsx
@@ -1,17 +1,18 @@
 import MessageForm from "../../../features/chat/component/MessageForm";
 import MessageList from "../../../features/chat/component/MessageList";
 import { useChat } from "../../../features/chat/hooks/useChat";
+import "./ChatPage.css";
 
 function ChatPage() {
   const { messages, sendMessage, isConnected } = useChat();
 
   return (
     <section className="dashboard-section ">
-      <article className="partSections">
+      <article className="partSections chat-column">
         <header className="headerPartSection">
           <h3 className="dashboard-layout-h3">Chat</h3>
         </header>
-        <div className="message-container" data-project-id="0">
+        <div className="message-container">
           <MessageList messages={messages} />
           <MessageForm />
         </div>
