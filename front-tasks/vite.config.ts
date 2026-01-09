29f638db72bc76101d1028f4030c06a170d0f01f
SrStamm <mirkostamm19@gmail.com>
2026-01-09 12:42:43 +0000

    updated config

--- front-tasks/vite.config.ts
+++ front-tasks/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// Versión más simple que definitivamente funciona
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: "dist",
     sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom", "react-router-dom"],
+        },
+      },
+    },
   },
 });
