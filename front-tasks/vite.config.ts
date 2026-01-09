3580232a4b687a0f9f674f533b7d6f9b43bcaa82
SrStamm <mirkostamm19@gmail.com>
2026-01-09 12:19:41 +0000

    config updated

--- front-tasks/vite.config.ts
+++ front-tasks/vite.config.ts
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react({
+      babel: {
+        plugins: [["babel-plugin-react-compiler", {}]],
+      },
+    }),
+  ],
+  build: {
+    sourcemap: false, // Desactiva source maps en producción
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true, // Elimina console.log en producción
+        drop_debugger: true, // Elimina debugger
+      },
+    },
+  },
+  esbuild: {
+    drop: ["console", "debugger"], // Elimina console y debugger
+  },
+});
