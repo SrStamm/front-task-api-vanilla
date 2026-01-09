06371a074d84178e45a0bf1fc1a7e5960d9d7c8d
SrStamm <mirkostamm19@gmail.com>
2026-01-09 22:28:20 +0000

    fix: issue to call API in GroupService fixed

--- front-tasks/src/features/groups/api/GroupService.ts
+++ front-tasks/src/features/groups/api/GroupService.ts
@@ -8,7 +8,6 @@ import { AddRemoveUserToGroup, UpdateGroup } from "../../../types/Group.ts";
 export async function fetchGroups() {
   const res = await Fetch({ path: "group", method: "GET" });
 
-  if (!res.ok) throw new Error("Failed to fetch groups");
   return res.json();
 }
 
@@ -20,14 +19,12 @@ export async function fetchGroupsMe() {
 export async function fetchUsersGroup(group_id: number) {
   const res = await Fetch({ path: `group/${group_id}/users`, method: "GET" });
 
-  if (!res.ok) throw new Error("Failed to fetch user in group");
   return res;
 }
 
 export async function createGroupApi(data: CreateGroupInterface) {
   const res = await Fetch({ path: "group", method: "POST", body: data });
 
-  if (!res.ok) throw new Error("Failed to create group");
   return res;
 }
 
@@ -38,14 +35,12 @@ export async function updateGroupApi(group_id: number, data: UpdateGroup) {
     body: data,
   });
 
-  if (!res.ok) throw new Error("Failed to update group");
   return res;
 }
 
 export async function deleteGroupApi(group_id: number) {
   const res = await Fetch({ path: `group/${group_id}`, method: "DELETE" });
 
-  if (!res.ok) throw new Error("Failed to delete group");
   return res;
 }
 
@@ -58,7 +53,6 @@ export async function addUserToGroupApi({
     method: "POST",
   });
 
-  if (!res.ok) throw new Error("Failed to add user to group");
   return res;
 }
 
@@ -71,7 +65,6 @@ export async function removeUserToGroup({
     method: "DELETE",
   });
 
-  if (!res.ok) throw new Error("Failed to remove user to group");
   return res;
 }
 
@@ -90,6 +83,5 @@ export async function updateUserToGroup({
     body: roleData,
   });
 
-  if (!res.ok) throw new Error("Failed to update role for user to group");
   return res;
 }
