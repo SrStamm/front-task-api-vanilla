0e7146656b11d91c9257dba1b3e235f4a126bbe1
SrStamm <mirkostamm19@gmail.com>
2026-01-10 22:47:40 +0000

    style: Button and Modal

--- front-tasks/src/features/projects/components/ProjectModal/index.tsx
+++ front-tasks/src/features/projects/components/ProjectModal/index.tsx
@@ -26,7 +26,7 @@ function ProjectModal({
   const [tabSelected, setTabSelected] = useState("members");
   const { removeUserFromProject } = useProjects();
 
-  const header = <h2 className="modal-title">{project.title}</h2>;
+  const header = <h2>{project.title}</h2>;
 
   const body = (
     <>
