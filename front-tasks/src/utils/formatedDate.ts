9e2fceb0af6d568b7209f3e48e24908ff9d3bb26
SrStamm <mirkostamm19@gmail.com>
2026-01-12 21:08:17 +0000

    feat: show date in different formats

--- front-tasks/src/utils/formatedDate.ts
+++ front-tasks/src/utils/formatedDate.ts
@@ -1,11 +1,34 @@
 function formatDate(dateString: any) {
-  const options: Intl.DateTimeFormatOptions = {
-    year: "numeric",
-    month: "2-digit",
-    day: "2-digit",
-  };
+  const date = new Date(dateString);
 
-  return dateString.toLocaleDateString("es-ES", options);
+  const currentDate = new Date();
+  const year = currentDate.getFullYear();
+  const mes = currentDate.getMonth(); // Enero es 0
+  const dia = currentDate.getDate();
+
+  let options: Intl.DateTimeFormatOptions = {};
+
+  if (date.getFullYear() == year) {
+    if (date.getMonth() == mes && date.getDate() == dia) {
+      options = {
+        hour: "2-digit",
+        minute: "2-digit",
+      };
+    } else {
+      options = {
+        month: "2-digit",
+        day: "2-digit",
+      };
+    }
+  } else {
+    options = {
+      year: "numeric",
+      month: "2-digit",
+      day: "2-digit",
+    };
+  }
+
+  return date.toLocaleTimeString("es-ES", options);
 }
 
 export default formatDate;
