a3b94ce2478e4e463f53ff679c747f6ed4eddb3b
SrStamm <mirkostamm19@gmail.com>
2026-01-18 20:35:46 +0000

    feat: change theme between dark and light

--- front-tasks/src/components/ui/UserOptions/index.tsx
+++ front-tasks/src/components/ui/UserOptions/index.tsx
@@ -1,10 +1,14 @@
 import Button from "../../common/Button";
 import { AuthContext } from "../../../providers/AuthProvider";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 
 import "./UserOptions.css";
 
 function UserOptions() {
+  const [isDark, setIsDark] = useState(
+    localStorage.getItem("theme") === "dark",
+  );
+
   const authContext = useContext(AuthContext);
 
   const handleLogout = async () => {
@@ -12,6 +16,28 @@ function UserOptions() {
       authContext?.logout();
     } catch (error) {
       console.error("Error al intentar conectar con el servidor:", error);
+    }
+  };
+
+  useEffect(() => {
+    const root = document.documentElement;
+
+    if (isDark) {
+      root.classList.remove("light-theme");
+      localStorage.setItem("theme", "dark");
+    } else {
+      root.classList.add("light-theme");
+      localStorage.setItem("theme", "light");
+    }
+  }, [isDark]);
+
+  const handleChangeTheme = () => {
+    if (isDark) {
+      setIsDark(false);
+      localStorage.setItem("theme", "light");
+    } else {
+      setIsDark(true);
+      localStorage.setItem("theme", "dark");
     }
   };
 
@@ -27,7 +53,12 @@ function UserOptions() {
         </li>
         <li>
           <label className="switch">
-            <input type="checkbox" />
+            <input
+              type="checkbox"
+              aria-label="cambiar tema"
+              checked={isDark}
+              onChange={handleChangeTheme}
+            />
             <span className="slider"></span>
           </label>
         </li>
